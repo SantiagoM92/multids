156e9c68fd3b2f5e87fc1da93c22b79514fa7145
jmartinez <jairo.martinez@modinter.com.ec>
2021-01-27 21:53:15 -0500

    Initial commit

---
+++ src/main/java/com/projects/jsmu/multids/MultidsApplication.java
@@ -0,0 +1,13 @@
+package com.projects.jsmu.multids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/jsmu/multids/MultidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.jsmu.multids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
